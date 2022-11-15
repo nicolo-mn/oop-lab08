f57756021e62c3c34c69f57e0e36d6936e91333a
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2022-11-15 12:15:49 +0100

    implementata classe SimpleGUI

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,12 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.BorderLayout;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +14,32 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private final static int PROPORTION = 4;
     private final JFrame frame = new JFrame();
+
+    public SimpleGUI() {
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        JTextArea textArea = new JTextArea();
+        panel.add(textArea, BorderLayout.CENTER);
+        JButton saveButton = new JButton("Save");
+        panel.add(saveButton, BorderLayout.SOUTH);
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void dispay() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static final void main(String[] args) {
+        new SimpleGUI().dispay();
+    }
+
 
 }
