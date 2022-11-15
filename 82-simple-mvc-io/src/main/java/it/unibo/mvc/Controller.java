f3a0edd0b4e676acc18553e166e31cc143c0ca44
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2022-11-15 11:44:58 +0100

    svolta l'implementazione della classe Controller nel secondo esercizio

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,32 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    static private final String DEFAULT_FILE = System.getProperty("user.home") + System.getProperty("file.separator") + "output.txt";
+    
+    File currentFile;
+
+    public Controller() {
+        currentFile = new File(DEFAULT_FILE);
+    }
+
+    void setCurrentFile(final File file) {
+        this.currentFile = file;
+    }
+
+    File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    String getCurrentFilePath() {
+        return this.currentFile.getPath();
+    }
+
+    void saveToFile(String line) {
+    }
 
 }
